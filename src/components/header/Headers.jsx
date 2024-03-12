e020e3d591ef9d8d5250dadd8a33b89920df913f
rifathasan <mdmehedi1111bd@gmail.com>
2024-03-12 21:07:47 +0600

    basic react statcture added

---
+++ src/components/header/Headers.jsx
@@ -0,0 +1,11 @@
+
+
+const Headers = () => {
+    return (
+        <div>
+               <h1 className='text-5xl font-bold text-center'>Knowladeg Cafi</h1>
+        </div>
+    );
+};
+
+export default Headers;
